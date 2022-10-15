ecc8b4967ad06a0707692720fc96dc9dd8b4b228
Santiago <santiagoed1707mail.com.com>
2022-10-15 14:41:23 -0500

    Se agregan archivos

---
+++ Cajero/src/main/java/com/mycompany/cajero/Cajero.java
@@ -0,0 +1,28 @@
+
+package com.mycompany.cajero;
+
+import java.util.Scanner;
+
+public class Cajero {
+
+    int consultar, retirar, movimientos;
+    private int saldo;
+    Scanner entrada = new Scanner(System.in);
+    
+    public void Menu(){
+        int bandera = 0;
+        int opcion = 0;
+        
+        do{
+            System.out.println("Seleccione una de las opciones:");
+            System.out.println(" 1. CONSULTAR SALDO. ");
+            System.out.println(" 2. CONSULTAR SALDO. ");
+            
+        }while(bandera == 0)
+    }
+    
+    
+    public static void main(String[] args) {
+        
+    }
+}
